a2cf62881c352581b2982674253ec89370594222
Marcele <marcele2608@gmail.com>
2024-03-15 07:59:45 -0300

    html e css do footer feitos

---
+++ src/styleLoader.js
@@ -0,0 +1,8 @@
+const loadStyle = (stylePath) => {
+    const link = document.createElement("link");
+    link.rel = "stylesheet";
+    link.href = stylePath;
+    document.querySelector('head').appendChild(link);
+};
+
+export default loadStyle;
