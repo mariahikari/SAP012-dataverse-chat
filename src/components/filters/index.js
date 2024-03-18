c65116ed533ce90410afbe94884e7e8436f950a2
mariahikari <mariahikariyps@gmail.com>
2024-03-18 09:58:35 -0300

    atualização

--- src/components/filters/index.js
+++ src/components/filters/index.js
@@ -4,10 +4,10 @@ import { renderItems } from "../cards/index.js"
 import data from '../../data/dataset.js';
 
 export const filters = () => {
-    loadStyle("./components/filters/style.css");
-    const filtersEl = document.createElement("div");
-    filtersEl.classList.add("filtros");
-    filtersEl.innerHTML = `
+  loadStyle("./components/filters/style.css");
+  const filtersEl = document.createElement("div");
+  filtersEl.classList.add("filtros");
+  filtersEl.innerHTML = `
     <h2>Personalize sua busca:</h2>
     <label for="filtroGenero">Gênero:</label> 
     <select id="filtroGenero" name="genero" data-testid="select-filter">
@@ -34,83 +34,83 @@ export const filters = () => {
       <option value="asc">A-Z</option>
       <option value="desc">Z-A</option>
     </select>
+
+    <button id="resetButton" data-testid="button-clear">Mostrar Todos</button> <!-- Botão para redefinir a busca -->
+    <div id="quantidadeDeLivros"></div>
+    <section id="root"></section>
+
   </div>
 
     `;
+  
 
-    return filtersEl
-
-
-
-    
-const filtroGenero = document.querySelector('#filtroGenero');
-const filtroPreco = document.querySelector('#filtroPreco');
-const ordenarPor = document.querySelector('#ordenarPor');
-const resetButton = document.querySelector('#resetButton');
-const cardsLivros = document.querySelector('#root');
-const quantidadeDeLivros = document.querySelector('#quantidadeDeLivros');
-
-// Função para renderizar os livros com base nos dados fornecidos
-const renderizarLivros = (dados) => {
-  cardsLivros.innerHTML = ''; // Limpa o conteúdo atual
-  if (dados.length === 0) {
-    const semResultado = document.createElement('div');
-    semResultado.textContent = 'Parece que estamos com uma estante meio vazia! Que tal ajustar os filtros ou explorar outros gêneros para encontrar sua próxima leitura favorita?';
-    semResultado.classList.add('mensagem-sem-resultado');
-    cardsLivros.appendChild(semResultado);
-  }
-  else {
-    cardsLivros.appendChild(renderItems(dados)); // Renderiza os livros
-  }
-
-};
-
-// Função para filtrar e ordenar os livros com base nos filtros e na ordem selecionada
-const filtrarEOrdenarLivros = () => {
-  // Obtém os valores selecionados dos filtros
-  const generoSelecionado = filtroGenero.value;
-  const precoSelecionado = filtroPreco.value;
-  const ordenacaoSelecionada = ordenarPor.value;
-
-  // Filtrar os dados com base no gênero e no preço
-  let dadosFiltrados = data;
-  if (generoSelecionado !== 'todos') {
-    dadosFiltrados = filterData(data, 'generoLivro', generoSelecionado);
-  }
-
-  if (precoSelecionado !== 'todos') {
-    dadosFiltrados = filterData(dadosFiltrados, 'precoMedio', precoSelecionado);
-  }
-
-  // Ordenar os dados de acordo com a opção selecionada
-  dadosFiltrados = sortData(dadosFiltrados, 'name', ordenacaoSelecionada);
-
-  renderizarLivros(dadosFiltrados); // Renderiza os livros filtrados e ordenados
-
-  const totalLivros = computeStats(dadosFiltrados);
-  quantidadeDeLivros.textContent = `Exibindo ${totalLivros} livro(s)`;
-};
+  const filtroGenero = document.querySelector('#filtroGenero');
+  const filtroPreco = document.querySelector('#filtroPreco');
+  const ordenarPor = document.querySelector('#ordenarPor');
+  const resetButton = document.querySelector('#resetButton');
+  const cardsLivros = document.querySelector('#root');
+  const quantidadeDeLivros = document.querySelector('#quantidadeDeLivros');
 
 
-renderizarLivros(data); // Renderiza todos os livros inicialmente
+  const renderizarLivros = (dados) => {
+    cardsLivros.innerHTML = '';
+    if (dados.length === 0) {
+      const semResultado = document.createElement('div');
+      semResultado.textContent = 'Parece que estamos com uma estante meio vazia! Que tal ajustar os filtros ou explorar outros gêneros para encontrar sua próxima leitura favorita?';
+      semResultado.classList.add('mensagem-sem-resultado');
+      cardsLivros.appendChild(semResultado);
+    }
+    else {
+      cardsLivros.appendChild(renderItems(dados));
+    }
 
-filtroGenero.addEventListener('change', () => filtrarEOrdenarLivros()); // Event listener para o filtro de gênero
-
-filtroPreco.addEventListener('change', () => filtrarEOrdenarLivros()); // Event listener para o filtro de preço
-
-ordenarPor.addEventListener('change', () => filtrarEOrdenarLivros()); // Event listener para o seletor de ordenação
+  };
 
 
-resetButton.addEventListener('click', (event) => { // Event listener para o botão de reset
-  console.log(event); //passar no teste - Palomita
-  // Define os valores padrão para os filtros
-  filtroGenero.value = 'todos';
-  filtroPreco.value = 'todos';
-  ordenarPor.value = 'asc'; // Define a ordem padrão para ascendente ao redefinir os filtros
-  quantidadeDeLivros.textContent = '';
+  const filtrarEOrdenarLivros = () => {
 
-  renderizarLivros(data); // Renderiza todos os livros novamente
-});
+    const generoSelecionado = filtroGenero.value;
+    const precoSelecionado = filtroPreco.value;
+    const ordenacaoSelecionada = ordenarPor.value;
+
+
+    let dadosFiltrados = data;
+    if (generoSelecionado !== 'todos') {
+      dadosFiltrados = filterData(data, 'generoLivro', generoSelecionado);
+    }
+
+    if (precoSelecionado !== 'todos') {
+      dadosFiltrados = filterData(dadosFiltrados, 'precoMedio', precoSelecionado);
+    }
+
+    dadosFiltrados = sortData(dadosFiltrados, 'name', ordenacaoSelecionada);
+
+    renderizarLivros(dadosFiltrados);
+
+    const totalLivros = computeStats(dadosFiltrados);
+    quantidadeDeLivros.textContent = `Exibindo ${totalLivros} livro(s)`;
+  };
+
+
+  renderizarLivros(data);
+
+  filtroGenero.addEventListener('change', () => filtrarEOrdenarLivros());
+
+  filtroPreco.addEventListener('change', () => filtrarEOrdenarLivros());
+
+  ordenarPor.addEventListener('change', () => filtrarEOrdenarLivros());
+
+
+  resetButton.addEventListener('click', (event) => {
+    filtroGenero.value = 'todos';
+    filtroPreco.value = 'todos';
+    ordenarPor.value = 'asc';
+    quantidadeDeLivros.textContent = '';
+
+    renderizarLivros(data);
+  });
+
+  return filtersEl
 
 };
 
