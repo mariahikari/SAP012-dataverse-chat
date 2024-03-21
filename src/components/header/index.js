7660f36c6ed5b149db879472f49f72c29e7b4993
Marcele <marcele2608@gmail.com>
2024-03-20 21:29:18 -0300

    modal e estilização

--- src/components/header/index.js
+++ src/components/header/index.js
@@ -9,17 +9,32 @@ export const header = () => {
     <p>Conecte-se e celebre histórias escritas por mulheres extraordinárias</p> 
     <nav>
       <ul>
-        <li>
-          <a href="/">Home</a>
-        </li>
-        <li>
-          <a href="/Chat">Chat</a>
-        </li>
+        <button class="btn" onclick="window.location.href='/'">Home</button>
+        <button class="btn" onclick="window.location.href='/Chat'">Chat</button>
+        <button class="btnKey" onclick="openModal()">API KEY</button>
       </ul>
     </nav>
+    <div id="myModal" class="modal">
+      <div class="modal-content">
+        <span class="close" onclick="closeModal()">x</span>
+        <h3>Inserir API KEY</h3>
+        <div class="input__modal">
+          <input type="text" id="input-modal" />
+          <button id="btn-modal">SALVAR</button>
+        </div>
+      </div>
+    </div>
     `;
-    
-  return headerEl;
 
+  return headerEl;
 }
 
+// Função para abrir o modal
+window.openModal = function openModal() {
+  document.getElementById("myModal").style.display = "block";
+}
+
+// Função para fechar o modal
+window.closeModal = function closeModal() {
+  document.getElementById("myModal").style.display = "none";
+}
