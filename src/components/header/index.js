c4b78e341a0a396eea8b818280eac94fb02c39aa
mariahikari <mariahikariyps@gmail.com>
2024-03-21 10:08:32 -0300

    Modal separado do header em component

--- src/components/header/index.js
+++ src/components/header/index.js
@@ -1,3 +1,4 @@
+import { modal } from "../modal/index.js";
 import loadStyle from "../../styleLoader.js";
 
 export const header = () => {
@@ -14,27 +15,13 @@ export const header = () => {
         <button class="btnKey" onclick="openModal()">API KEY</button>
       </ul>
     </nav>
-    <div id="myModal" class="modal">
-      <div class="modal-content">
-        <span class="close" onclick="closeModal()">x</span>
-        <h3>Inserir API KEY</h3>
-        <div class="input__modal">
-          <input type="text" id="input-modal" />
-          <button id="btn-modal">SALVAR</button>
-        </div>
-      </div>
-    </div>
     `;
 
+    document.addEventListener("DOMContentLoaded", () => {
+    const openModalBtn = headerEl.querySelector("openModalBtn");
+    openModalBtn.addEventListener("click", () => {
+      openModal();
+    });
+  });
   return headerEl;
-}
-
-// Função para abrir o modal
-window.openModal = function openModal() {
-  document.getElementById("myModal").style.display = "block";
-}
-
-// Função para fechar o modal
-window.closeModal = function closeModal() {
-  document.getElementById("myModal").style.display = "none";
 }
--- src/views/Home.js
+++ src/views/Home.js
@@ -1,10 +1,7 @@
 import { header } from '../components/header/index.js';
 import { footer } from "../components/footer/index.js";
 import { filters } from "../components/filters/index.js";
-import { renderItems } from "../components/cards/index.js";
-// import { filterData, sortData, computeStats } from "../../lib/dataFunctions.js";
 
-import data from '../../data/dataset.js';
 
 export const Home = () => {
   const rootElements = document.getElementById("root");
