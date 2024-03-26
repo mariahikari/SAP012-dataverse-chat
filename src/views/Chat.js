a020e9f68d1092d284e303e9b20558e80214d356
mariahikari <mariahikariyps@gmail.com>
2024-03-26 08:30:46 -0300

    livros renderizados no chat

--- src/views/Chat.js
+++ src/views/Chat.js
@@ -1,23 +1,29 @@
 import loadStyle from "../../styleLoader.js";
-// import data from "../data/dataset.js";
-// import { header } from '../components/header/index.js';
-// import { footer } from "../components/footer/index.js";
+import data from "../data/dataset.js";
+import { header } from '../components/header/index.js';
+import { footer } from "../components/footer/index.js";
 
 
 export const Chat = () => {
   const el = document.createElement('div');
+  const rootElements = document.getElementById("root");
+  
   loadStyle("./views/chat/style.css")
   
   el.innerHTML = `
       <div class="container">
           <div class="book-list">
           <h2>Livros</h2>
+          <ul id="bookList">
 
+          </ul>
           </div>
+
           <div class="book-chat">
               <div id="conversation">
                   <p>Clique em um livro para começar a conversar</p>
               </div>
+
               <div>
                   <input type="text" id="message" placeholder="Digite sua mensagem">
                   <button>Enviar</button>
@@ -25,12 +31,30 @@ export const Chat = () => {
           </div>
       </div>
   `;
+
+  rootElements.insertAdjacentElement("beforebegin", header());
+  rootElements.insertAdjacentElement("afterend", footer());
+
+ const renderChat = (data) => {
+    const bookList =el.querySelector('#bookList');
+    data.forEach((item) => {
+      const bookChat = document.createElement("li");
+      bookChat.innerHTML += ` 
+          <img src=${item.imageUrl} alt="Capa do Livro"/>
+          <p>${item.name}</p>
+      `;
+      bookList.appendChild(bookChat);
+    });
+  };
   
+  renderChat(data);
+
   return el;
+
   }
 
+ 
 
-//const rootElements = document.getElementById("root");
-//rootElements.insertAdjacentElement("beforebegin", header());
-//rootElements.insertAdjacentElement("afterend", footer());
+
+
   
