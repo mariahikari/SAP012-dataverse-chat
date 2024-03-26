9c6b01de7cbaffcb61a1622c32d69ca3e316d0c2
Marcele <marcele2608@gmail.com>
2024-03-26 07:22:33 -0300

    html base

--- src/views/Chat.js
+++ src/views/Chat.js
@@ -1,6 +1,36 @@
-export const Chat = () => {
-  const el = document.createElement('h2');
-  el.innerHTML = "Chat com os escritores";
-  return el;
-}
+import loadStyle from "../../styleLoader.js";
+// import data from "../data/dataset.js";
+// import { header } from '../components/header/index.js';
+// import { footer } from "../components/footer/index.js";
 
+
+export const Chat = () => {
+  const el = document.createElement('div');
+  loadStyle("./views/chat/style.css")
+  
+  el.innerHTML = `
+      <div class="container">
+          <div class="book-list">
+          <h2>Livros</h2>
+
+          </div>
+          <div class="book-chat">
+              <div id="conversation">
+                  <p>Clique em um livro para começar a conversar</p>
+              </div>
+              <div>
+                  <input type="text" id="message" placeholder="Digite sua mensagem">
+                  <button>Enviar</button>
+              </div>
+          </div>
+      </div>
+  `;
+  
+  return el;
+  }
+
+
+//const rootElements = document.getElementById("root");
+//rootElements.insertAdjacentElement("beforebegin", header());
+//rootElements.insertAdjacentElement("afterend", footer());
+  
